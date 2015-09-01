fcf605aa8e187a38443d5ba48a4f477adfa2978f
MrOnlineCoder <glinka1202@gmail.com>
2015-09-01 19:47:48 +0300

    server.js for Heroku update

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/ping', function (req, res) {
     res.end("Europe server");
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
     var host = server.address().address;
     var port = server.address().port;
 
