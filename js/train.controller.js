faf7528ebf6d32828e618be5c1e616f5f4647206
MrOnlineCoder <glinka1202@gmail.com>
2015-09-01 16:43:25 +0300

    Removed time setting, all english alphabet

--- js/train.controller.js
+++ js/train.controller.js
@@ -11,7 +11,7 @@
 
     function TrainController($routeParams, $timeout) {
         var vm = this;
-        vm.letters = ["a","b","c","d","h"];
+        vm.letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
         vm.text = "Click the textbox when you are ready";
         vm.theStyle = "background-color:  orangered; text-align: center";
         vm.correct = 0;
