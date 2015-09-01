085c04487b7ae3aa8a671063ee7abcdf73f9ec3d
MrOnlineCoder <glinka1202@gmail.com>
2015-09-01 21:00:04 +0300

    Created findgame page, images added

--- public/js/findgame.controller.js
+++ public/js/findgame.controller.js
@@ -12,20 +12,45 @@
         function FindGameController($http) {
             var vm = this;
             vm.connected = false;
+            vm.finding = false;
 
             vm.connectStatus = "Connecting to server...";
 
+            vm.findGame = function() {
+                vm.checkConnection();
+                vm.finding = true;
+                $http.post('/find', {nick:vm.nickname})
+                    .success(function(response) {
+                        // this callback will be called asynchronously
+                        // when the response is available
+                        vm.connectStatus = "You are in lobby";
+                        vm.connected = true;
+                    })
+                    .error(function(response) {
+                        vm.connectStatus = "No server connection. Multiplayer disabled";
+                        vm.connected = false;
+                        vm.finding = false;
+                    })
+            };
 
-            $http.get('http://localhost:3000/ping')
-                .success(function(response) {
-                    // this callback will be called asynchronously
-                    // when the response is available
-                    vm.connectStatus = "Connected to  "+response;
-                    vm.connected = true;
-                })
-                .error(function(response) {
-                    vm.connectStatus = "No server connection. Multiplayer disabled";
-            })
+            
+            vm.checkConnection = function () {
+                $http.get('/ping')
+                    .success(function(response) {
+                        // this callback will be called asynchronously
+                        // when the response is available
+                        vm.connectStatus = "Connected to  "+response;
+                        vm.connected = true;
+                    })
+                    .error(function(response) {
+                        vm.connectStatus = "No server connection. Multiplayer disabled";
+                        vm.connected = false;
+                    })
+            };
+
+            vm.checkConnection();
+
+
 
         }
 
--- server.js
+++ server.js
@@ -9,13 +9,26 @@ var staticServe = require("serve-static");
 var path = require('path');
 
 
-app.use(bodyParser.json()); // стандартный модуль, для парсинга JSON в запросах
+
+
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
 //app.use(app.router); // модуль для простого задания обработчиков путей
 app.use(express.static(path.join(__dirname, 'public'))); // запуск статического файлового сервера, который смотрит на папку public/ (в нашем случае отдает index.html)
 
 
+var players = [];
+
+
 app.get('/ping', function (req, res) {
     res.end("Europe server");
+});
+
+app.post('/find', function (req, res) {
+    players.push(req.body.nick);
+    console.log("- Client is finding game: "+req.body.nick);
 });
 
 var server = app.listen(process.env.PORT || 3000, function () {
